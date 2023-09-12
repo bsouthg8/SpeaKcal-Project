fc0abcd80d3d67646bd8656d52182b63f9b40b48
anikakanizesha <105477189+anikakanizesha@users.noreply.github.com>
2023-09-13 00:13:52 +1200

    commit to test_test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     id("com.android.application")
+    // Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.speakcalproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.speakcalproject"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -31,6 +33,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
@@ -43,7 +46,23 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment:2.7.1")
     implementation("androidx.navigation:navigation-ui:2.7.1")
+    implementation("com.google.firebase:firebase-firestore:24.7.1")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("com.google.android.gms:play-services-tasks:18.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
 }
--- app/src/main/java/com/example/speakcalproject/Login.java
+++ app/src/main/java/com/example/speakcalproject/Login.java
@@ -2,13 +2,72 @@ package com.example.speakcalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Login extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+
+    // Replace this list with a database or backend service.
+    private List<User> userList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // Replace this with actual user authentication logic.
+                User user = authenticateUser(email, password);
+
+                if (user != null) {
+                    // Login successful
+                    // You can store user data in SharedPreferences or perform other actions.
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Login failed
+                    Toast.makeText(Login.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // Replace this with actual authentication logic.
+    @SuppressLint("RestrictedApi")
+    private User authenticateUser(String email, String password) {
+        for (User user : userList) {
+            if (user.getUid().equals(email) && user.getClass().equals(password)) {
+                return user;
+            }
+        }
+        return null; // Authentication failed
+    }
+
+    public void openSignupActivity(View view) {
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
     }
 }
