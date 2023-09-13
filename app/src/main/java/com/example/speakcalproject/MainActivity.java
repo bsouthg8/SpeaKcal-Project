ee325aafba4cc7ecbcdaa5f4b24bd28db35c18d6
James Dunlop <jamezdunlop@gmail.com>
2023-09-13 12:38:58 +1200

    this is my commit

--- app/src/main/java/com/example/speakcalproject/MainActivity.java
+++ app/src/main/java/com/example/speakcalproject/MainActivity.java
@@ -12,26 +12,64 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.speakcalproject.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+            private static final String TAG = "MainActivity";
+            private FirebaseFirestore firestore;
+            private ListView breakfastOutput;
+            private ArrayList<String> list;
+            private ArrayAdapter<String> adapter;
+            private EditText enterBreakfast;
+            private Button add;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+                // Initialize Firestore
+                firestore = FirebaseFirestore.getInstance();
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+                // Initialize ListView and ArrayAdapter
+                breakfastOutput = findViewById(R.id.Output_Breakfast);
+                list = new ArrayList<>();
+                adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+                breakfastOutput.setAdapter(adapter);
+
+                enterBreakfast = findViewById(R.id.Input_Breakfast);
+                add = findViewById(R.id.Input_Food);
+
+                add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String food = enterBreakfast.getText().toString();
+                        retrieveFoodFromDatabase(food);
+                    }
+                });
+            }
+
+            private void retrieveFoodFromDatabase(String food) {
+                // Retrieve the typed food from the Firestore database
+                firestore.collection("Food")
+                        .whereEqualTo("foodName", food)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    list.clear(); // Clear the current list
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String foodName = document.getString("foodName");
+                                        list.add(foodName); // Add the retrieved food to the list
+                                    }
+                                    adapter.notifyDataSetChanged(); // Notify the adapter about the data change
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+            }
+        }
 
 }
