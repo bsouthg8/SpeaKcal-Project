1729e77928c1363efac161031cab639a4360f400
y7k9h8d9 <56863443+y7k9h8d9@users.noreply.github.com>
2023-09-18 23:53:45 +1200

    starts to work on MainActivity

--- app/src/main/java/com/example/speakcalproject/MainActivity.java
+++ app/src/main/java/com/example/speakcalproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,33 +19,32 @@ import com.example.speakcalproject.databinding.ActivityMainBinding;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-    private Button goToPhotoRecognition;
+    private TextView textView;
+    private ImageView imageView;
+    private String userName;
 
-    FirebaseFirestore firestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-
+        setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        UserDatabaseManagement.getUserData(getApplicationContext(), new UserDatabaseManagement.OnUserDataCallback() {
+            @Override
+            public void onUserDataReceived(Map<String, Object> userData) {
+                userName = (String) userData.get("username");
+                textView.setText("Welcome back\n"+userName);
+            }
+        });
 
 
         // The following code is test code to see if adding data to the database is working properly
@@ -67,5 +68,7 @@ public class MainActivity extends AppCompatActivity {
         }); // database test ends here */
 
     }
+
+
 
 }
