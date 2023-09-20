1ada76fdc09a81a124e698dca288de7096ce0434
James Dunlop <jamezdunlop@gmail.com>
2023-09-20 14:56:35 +1200

    its not finnished

--- app/src/main/java/com/example/speakcalproject/Journal_entry.java
+++ app/src/main/java/com/example/speakcalproject/Journal_entry.java
@@ -1,5 +1,6 @@
 package com.example.speakcalproject;//package com.example.speakcalproject;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -7,11 +8,14 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,67 +26,74 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
+import java.util.LinkedList;
 
 
 public class Journal_entry extends AppCompatActivity {
 
-    private static final String TAG = "Journal_entry";
+    Button button;
+    //    TextView textView;
+    ListView listView;
+    EditText editText;
 
-
-    private ArrayList<String> list;
-    private ArrayAdapter<String> adapter;
-    private EditText enterBreakfast;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("Food");
+        DatabaseReference Ref = FirebaseDatabase.getInstance().getReference().child("Food");
 
-        enterBreakfast = findViewById(R.id.Input_Breakfast);
+        Ref.child("Name").setValue("milk");
+        Ref.child("Amount").setValue(23);
 
-        ListView breakfastOutput = findViewById(R.id.Output_Breakfast);
-        list = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        breakfastOutput.setAdapter(adapter);
+        button = findViewById(R.id.button_ID);
+//        textView = findViewById(R.id.textView_ID);
+        listView = findViewById(R.id.list_ID);
+        editText = findViewById(R.id.editText_ID);
 
-        Button add = findViewById(R.id.GetFromDatabase);
-        add.setOnClickListener(new View.OnClickListener() {
+        DatabaseReference foodRef = FirebaseDatabase.getInstance().getReference().child("Food");
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                LinkedList<Pair<String, Long>> foodList = new LinkedList<>();
+                String userInput = editText.getText().toString();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String name = snapshot.child("Name").getValue(String.class);
+                    Long amount = snapshot.child("Amount").getValue(Long.class);
+                    if (name != null && name.equals(userInput)) {
+                        foodList.add(new Pair<>(name, amount));
+                    }
+                }
+
+                ArrayAdapter<Pair<String, Long>> adapter = new ArrayAdapter<Pair<String, Long>>(Journal_entry.this, android.R.layout.simple_list_item_2, android.R.id.text1, foodList) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+                        Pair<String, Long> pair = (Pair<String, Long>) getItem(position);
+                        TextView text1 = view.findViewById(android.R.id.text1);
+                        TextView text2 = view.findViewById(android.R.id.text2);
+                        if (pair != null) {
+                            text1.setText(pair.first);
+                            text2.setText(String.valueOf(pair.second));
+                        }
+                        return view;
+                    }
+                };
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Log a message
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String foodName = enterBreakfast.getText().toString();// Gets user input
-                Query query = ref.orderByChild("Name").equalTo(foodName);// Query the database
-
-                query.addListenerForSingleValueEvent(new ValueEventListener() {
-
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        list.clear();
-                        //Retrievve data from the database
-                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                            String name = childSnapshot.child("Name").getValue(String.class);
-                            int amount = childSnapshot.child("Amount").getValue(Integer.class);
-                            String entry = "Name: " + name + "\nAmount: " + amount;
-                            list.add(entry);
-                            Log.d(TAG, "Data: " + entry); // Log the data
-                        }
-                        adapter.notifyDataSetChanged();
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                        Log.e(TAG, "Fail to retrive data", error.toException());
-                    }
-                });
+                foodRef.addListenerForSingleValueEvent(valueEventListener);
             }
         });
-
-
     }
 }
-
