f9e41d8434f78c2e4836ab2f5821b086f2bf34cf
bsouthg8 <bsouthg8@gmail.com>
2023-09-21 06:24:04 +1200

    some more changes
    just so that the text is always displayed on the nav bar

--- app/src/main/java/com/example/speakcalproject/MainActivity.java
+++ app/src/main/java/com/example/speakcalproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.speakcalproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             } else if (itemId == R.id.navigation_journal) {
                 Intent intent = new Intent(this, TestJournal.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                 return true;
