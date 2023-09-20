bdddee3536862db0fa9f9aee0826c5839338fbf3
James Dunlop <jamezdunlop@gmail.com>
2023-09-20 14:57:42 +1200

    its not finnished

--- app/src/main/java/com/example/speakcalproject/Journal_entry.java
+++ app/src/main/java/com/example/speakcalproject/Journal_entry.java
@@ -39,7 +39,7 @@ public class Journal_entry extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dailylog);
 
         DatabaseReference Ref = FirebaseDatabase.getInstance().getReference().child("Food");
 
