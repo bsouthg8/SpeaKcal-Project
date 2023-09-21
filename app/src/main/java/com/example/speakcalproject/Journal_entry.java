4b6c4fd06aead1e388fd1a2dd9474ae4941e6ad6
James Dunlop <jamezdunlop@gmail.com>
2023-09-22 11:42:24 +1200

    Jurnal_entry and dayly log are the ones ive change

--- app/src/main/java/com/example/speakcalproject/Journal_entry.java
+++ app/src/main/java/com/example/speakcalproject/Journal_entry.java
@@ -1,11 +1,7 @@
 package com.example.speakcalproject;//package com.example.speakcalproject;
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
@@ -18,13 +14,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Journal_entry extends AppCompatActivity {
 
@@ -38,17 +41,27 @@ public class Journal_entry extends AppCompatActivity {
     EditText editText2;
     EditText editText3;
 
+    FirebaseFirestore db;
+    CollectionReference foodRef;
+    ListenerRegistration listener1;
+    ListenerRegistration listener2;
+    ListenerRegistration listener3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dailylog);
+
+        // Initialize the Firestore database
+        db = FirebaseFirestore.getInstance();
+        foodRef = db.collection("Food");
 
         // Initialize the button
         button = findViewById(R.id.button_ID);
         button2 = findViewById(R.id.button2_ID);
         button3 = findViewById(R.id.button3_ID);
 
-        // Initialize the  ListView
+        // Initialize the ListView
         listView = findViewById(R.id.list_ID);
         listView2 = findViewById(R.id.list2_ID);
         listView3 = findViewById(R.id.list3_ID);
@@ -58,86 +71,120 @@ public class Journal_entry extends AppCompatActivity {
         editText2 = findViewById(R.id.editText2_ID);
         editText3 = findViewById(R.id.editText3_ID);
 
-        DatabaseReference foodRef = FirebaseDatabase.getInstance().getReference().child("Food");
-        ValueEventListener valueEventListener = new ValueEventListener() {
-
-            // onDataChane looks for any change in the data base and activates when the data in the database changes
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //arrayList stores the data from the data base that is equil to what is in the Text view
-                ArrayList<Pair<String, Long>> foodList = new ArrayList<>();
-                String userInput = editText.getText().toString();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String name = snapshot.child("Name").getValue(String.class);
-                    Long amount = snapshot.child("Amount").getValue(Long.class);
-                    if (name != null && name.equals(userInput)) {
-                        foodList.add(new Pair<>(name, amount));
-                    }
-                }
-
-                ArrayAdapter<Pair<String, Long>> adapter = new ArrayAdapter<Pair<String, Long>>(Journal_entry.this, android.R.layout.simple_list_item_2, android.R.id.text1, foodList) {
-
-                    //getView is responsible for returning a view
-                    @NonNull
-                    @Override
-                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                        View view = super.getView(position, convertView, parent);
-                        Pair<String, Long> pair = (Pair<String, Long>) getItem(position);
-                        TextView text1 = view.findViewById(android.R.id.text1);
-                        TextView text2 = view.findViewById(android.R.id.text2);
-                        if (pair != null) {
-                            text1.setText(pair.first);
-                            text2.setText(String.valueOf(pair.second));
-                        }
-                        return view;
-                    }
-                };
-                listView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Log or show the error message
-                Log.e("DatabaseError", databaseError.getMessage());
-            }
-        };
-
+        // For button1
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userInput = editText.getText().toString();
                 if (!userInput.isEmpty()) {
-                    foodRef.addListenerForSingleValueEvent(valueEventListener);
+                    if (listener1 != null) {
+                        listener1.remove();
+                    }
+                    listener1 = foodRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot querySnapshot, @Nullable FirebaseFirestoreException e) {
+                            handleEvent(querySnapshot, e, userInput, listView);
+                        }
+                    });
                 } else {
-//                    Toast.makeText(getApplicationContext(), "Input cannot be empty", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Input cannot be empty", Toast.LENGTH_SHORT).show();
                 }
+
+                // Reset the EditText field
+                editText.setText("");
             }
         });
 
+// For button2
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userInput2 = editText2.getText().toString();
-                if (!userInput2.isEmpty()) {
-                    foodRef.addListenerForSingleValueEvent(valueEventListener);
+                String userInput = editText2.getText().toString();
+                if (!userInput.isEmpty()) {
+                    if (listener2 != null) {
+                        listener2.remove();
+                    }
+                    listener2 = foodRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot querySnapshot, @Nullable FirebaseFirestoreException e) {
+                            handleEvent(querySnapshot, e, userInput, listView2);
+                        }
+                    });
                 } else {
-//                    Toast.makeText(getApplicationContext(), "Input cannot be empty", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Input cannot be empty", Toast.LENGTH_SHORT).show();
                 }
+
+                // Reset the EditText field
+                editText2.setText("");
             }
         });
 
+// For button3
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userInput2 = editText2.getText().toString();
-                if (!userInput2.isEmpty()) {
-                    foodRef.addListenerForSingleValueEvent(valueEventListener);
+                String userInput = editText3.getText().toString();
+                if (!userInput.isEmpty()) {
+                    if (listener3 != null) {
+                        listener3.remove();
+                    }
+                    listener3 = foodRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot querySnapshot, @Nullable FirebaseFirestoreException e) {
+                            handleEvent(querySnapshot, e, userInput, listView3);
+                        }
+                    });
                 } else {
-//                    Toast.makeText(getApplicationContext(), "Input cannot be empty", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Input cannot be empty", Toast.LENGTH_SHORT).show();
                 }
+
+                // Reset the EditText field
+                editText3.setText("");
             }
         });
+
     }
 
+
+    private void handleEvent(@Nullable QuerySnapshot querySnapshot, @Nullable FirebaseFirestoreException e, String userInput, ListView listView) {
+        if (e != null) {
+            Log.e("Fire store Error", Objects.requireNonNull(e.getMessage()));
+            return;
+        }
+
+        ArrayList<Pair<String, String>> foodList = new ArrayList<>();
+
+//        assert querySnapshot != null;
+//        foodList.clear(); // Clear the list before populating with new items
+
+        for (QueryDocumentSnapshot document : querySnapshot) {
+            String name = document.getString("Name");
+            String amount = document.getString("Amount");
+
+            if (name != null && name.equals(userInput)) {
+                foodList.add(new Pair<>("Food: " + name, "Calories: "+amount));
+            }
+        }
+
+        ArrayAdapter<Pair<String, String>> adapter = new ArrayAdapter<Pair<String, String>>(Journal_entry.this, android.R.layout.simple_list_item_2, android.R.id.text1, foodList) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                Pair<String, String> pair = getItem(position);
+                TextView text1 = view.findViewById(android.R.id.text1);
+                TextView text2 = view.findViewById(android.R.id.text2);
+
+                if (pair != null) {
+                    text1.setText(pair.first);
+                    text2.setText(pair.second);
+                }
+
+                return view;
+            }
+        };
+
+        listView.setAdapter(adapter);
+    }
 
 }
