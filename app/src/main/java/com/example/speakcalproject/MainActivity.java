1c42595bf7c7df78f96895b3fb8c3e5a92133c49
bsouthg8 <bsouthg8@gmail.com>
2023-09-22 23:24:00 +1200

    reverting changes

--- app/src/main/java/com/example/speakcalproject/MainActivity.java
+++ app/src/main/java/com/example/speakcalproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.speakcalproject;
 
 import android.app.Activity;
+<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,6 +11,11 @@ import android.widget.ImageView;
 
 import android.widget.Toast;
 
+=======
+import android.os.Bundle;
+import android.widget.Toast;
+
+>>>>>>> parent of d422ab8 (All Changes done)
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,7 +27,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.speakcalproject.databinding.ActivityMainBinding;
 import com.google.firebase.FirebaseApp;
+<<<<<<< HEAD
+=======
+import com.google.firebase.firestore.DocumentReference;
+>>>>>>> parent of d422ab8 (All Changes done)
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -29,57 +40,55 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
 
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
         FirebaseApp.initializeApp(this);
 
 
-        ImageButton meals = (ImageButton) findViewById(R.id.meals);
-        meals.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MealsActivity.class));
-            }
-        });
 
-        ImageButton recipes = (ImageButton) findViewById(R.id.recipes);
-        recipes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RecipeActivity.class));
-            }
-        });
+        // The following code is test code to see if adding data to the database is working properly
+        /* firestore = FirebaseFirestore.getInstance();
 
-        ImageButton groceries = (ImageButton) findViewById(R.id.groceries);
-        groceries.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, GroceriesActivity.class));
-            }
-        });
+        Map<String, Object> users = new HashMap<>();
+        users.put("firstName", "Bradley");
+        users.put("lastName", "Southgate");
+        users.put("description", "Hello");
 
-        ImageButton settings = (ImageButton) findViewById(R.id.settings);
-        settings.setOnClickListener(new View.OnClickListener() {
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Profile.class));
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
             }
-        });
+        }).addOnFailureListener(new OnFailureListener() {
+>>>>>>> parent of d422ab8 (All Changes done)
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+            }
+        }); // database test ends here */
 
     }
 
-    @Override
-    public void onBackPressed() {
-        moveTaskToBack(true);
-    }
 }
